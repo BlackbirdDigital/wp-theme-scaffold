5cc524e38670e70317084873e67891170fc03bcd
phil <hoytph@gmail.com>
2021-02-15 19:16:44 -0500

    Cleaner 404 and removed sidebar on page
    Debating removing sidebar all together.

--- 404.php
+++ 404.php
@@ -18,38 +18,7 @@ get_header();
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'theme-scaffold' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'theme-scaffold' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$theme_scaffold_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'theme-scaffold' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$theme_scaffold_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'theme-scaffold' ); ?></p>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
--- page.php
+++ page.php
@@ -34,5 +34,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
