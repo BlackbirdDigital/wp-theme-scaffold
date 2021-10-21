12901a6271558cd54de9c59a9177d4c60aaef0db
Cory Hughart <cory@blackbird.digital>
2021-10-21 18:05:54 -0400

    simplify get_component

--- inc/utilities.php
+++ inc/utilities.php
@@ -22,21 +22,16 @@ function debug( $var ) {
 }
 
 /**
- * Output a template from the components foluder using `get_template_part()`.
+ * Output a template from the components folder using `get_template_part()`.
  *
- * @param string       $slug Slug name of the module file.
- * @param string|array $name The name of the specialised template or arguments array.
- * @param array        $args Optional. Additional arguments passed to the template.
- *                           Not used if $name is an arguments array.
- *                           Default empty array.
+ * @param string $name Name of the component file.
+ * @param array  $args Optional. Additional arguments passed to the template.
+ *                     Not used if $name is an arguments array.
+ *                     Default empty array.
  * @return void|false Void on success, false if the template does not exist.
  */
-function get_component( $slug, $name = null, $args = array() ) {
-	if ( is_array( $name ) ) {
-		$args = $name;
-		$name = null;
-	}
-	return get_template_part( 'components/' . $slug, $name, $args );
+function get_component( $name, $args = array() ) {
+	return get_template_part( 'components/' . $name, null, $args );
 }
 
 /**
