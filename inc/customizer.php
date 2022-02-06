656413b9d3246d7952f0c9dfa5b98785d7012bd8
Cory Hughart <cory@blackbird.digital>
2022-02-06 13:28:35 -0500

    namespace customizer functions

--- inc/customizer.php
+++ inc/customizer.php
@@ -5,12 +5,14 @@
  * @package theme-scaffold
  */
 
+namespace ThemeScaffold\Customizer;
+
 /**
  * Add postMessage support for site title and description for the Theme Customizer.
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function theme_scaffold_customize_register( $wp_customize ) {
+function register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
@@ -20,26 +22,26 @@ function theme_scaffold_customize_register( $wp_customize ) {
 			'blogname',
 			array(
 				'selector'        => '.site-title a',
-				'render_callback' => 'theme_scaffold_customize_partial_blogname',
+				'render_callback' => __NAMESPACE__ . '\\partial_blogname',
 			)
 		);
 		$wp_customize->selective_refresh->add_partial(
 			'blogdescription',
 			array(
 				'selector'        => '.site-description',
-				'render_callback' => 'theme_scaffold_customize_partial_blogdescription',
+				'render_callback' => __NAMESPACE__ . '\\partial_blogdescription',
 			)
 		);
 	}
 }
-add_action( 'customize_register', 'theme_scaffold_customize_register' );
+add_action( 'customize_register', __NAMESPACE__ . '\\register' );
 
 /**
  * Render the site title for the selective refresh partial.
  *
  * @return void
  */
-function theme_scaffold_customize_partial_blogname() {
+function partial_blogname() {
 	bloginfo( 'name' );
 }
 
@@ -48,15 +50,15 @@ function theme_scaffold_customize_partial_blogname() {
  *
  * @return void
  */
-function theme_scaffold_customize_partial_blogdescription() {
+function partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function theme_scaffold_customize_preview_js() {
+function preview_js() {
 	$customizerjs_asset = require get_template_directory() . '/dist/js/customizer.asset.php';
 	wp_enqueue_script( 'theme-scaffold-customizer', get_template_directory_uri() . '/js/customizer.js', array_merge( array( 'customize-preview' ), $customizerjs_asset['dependencies'] ), $customizerjs_asset['version'], true );
 }
-add_action( 'customize_preview_init', 'theme_scaffold_customize_preview_js' );
+add_action( 'customize_preview_init', __NAMESPACE__ . '\\preview_js' );
