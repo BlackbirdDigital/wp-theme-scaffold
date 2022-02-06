46a267297f1c9f1af41b95e510de065c11079618
Cory Hughart <cory@blackbird.digital>
2022-02-06 13:39:43 -0500

    add archive title filter to remove "Archive: "

--- inc/hooks.php
+++ inc/hooks.php
@@ -11,7 +11,7 @@ namespace ThemeScaffold\Hooks;
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
- * @return array
+ * @return array Modified classes for the body element.
  */
 function body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
@@ -27,6 +27,28 @@ function body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', __NAMESPACE__ . '\\body_classes' );
+
+/**
+ * Remove "Archive: " prefix from archive titles.
+ *
+ * @param string $title Current archive title.
+ * @return string Modified archive title.
+ */
+function archive_title( $title ) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+	} elseif ( is_author() ) {
+		$title = '<span class="vcard">' . get_the_author() . '</span>';
+	} elseif ( is_tax() ) { // for custom post types
+		$title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+	} elseif ( is_post_type_archive() ) {
+		$title = post_type_archive_title( '', false );
+	}
+	return $title;
+}
+add_filter( 'get_the_archive_title', __NAMESPACE__ . '\\archive_title' );
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
