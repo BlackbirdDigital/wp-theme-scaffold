f0489645cf9a707f7409eee5b3930cad70e89775
Cory Hughart <cory@blackbird.digital>
2022-06-09 08:58:05 -0400

    add postcss plugins for inset & logical properties

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-	plugins: [ require( 'postcss-size' ), require('autoprefixer') ],
+	plugins: [
+		require( 'postcss-size' ),
+		require( 'postcss-inset' ),
+		require( 'postcss-logical' ),
+		require( 'autoprefixer' ),
+	],
 };
