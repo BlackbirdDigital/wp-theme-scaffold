5e04454e8f0a0c89c64c03bbcddc5cf90426a10e
Cory Hughart <cory@blackbird.digital>
2022-07-27 21:25:00 -0400

    move menu-toggle back outside site-navigation so that it isn't targeting it's own parent

--- template-parts/site/header.php
+++ template-parts/site/header.php
@@ -5,12 +5,26 @@
  * @package theme-scaffold
  */
 
+use function ThemeScaffold\Utilities\get_component;
+
+$main_nav_id = 'main-navigation';
 ?>
 
 <header id="masthead" class="site-header">
 	<nav class="site-header__nav">
 		<?php get_template_part( 'template-parts/site/branding' ); ?>
 
-		<?php get_template_part( 'template-parts/site/navigation', null, array( 'id' => 'main-navigation' ) ); ?>
+		<?php
+		get_component(
+			'menu-toggle',
+			array(
+				'id'            => 'main-navigation-toggle',
+				'aria-controls' => $main_nav_id,
+				'aria-expanded' => false,
+			)
+		);
+		?>
+
+		<?php get_template_part( 'template-parts/site/navigation', 'header', array( 'id' => $main_nav_id ) ); ?>
 	</nav>
 </header><!-- .site-header -->
--- template-parts/site/navigation.php
+++ template-parts/site/navigation.php
@@ -7,9 +7,8 @@
 
 use function ThemeScaffold\Utilities\array_subset;
 use function ThemeScaffold\Utilities\attributes_from_array;
-use function ThemeScaffold\Utilities\get_component;
 
-$default = array(
+$defaults = array(
 	'id' => null,
 );
 
@@ -19,23 +18,14 @@ $attributes = array_subset( $args, array( 'id' ) );
 ?>
 
 <div <?php attributes_from_array( $attributes ); ?> class="site-navigation">
-	<?php
-	get_component(
-		'menu-toggle',
-		array(
-			'id'            => 'main-navigation-toggle',
-			'aria-controls' => 'main-navigation',
-			'aria-expanded' => false,
-		)
-	);
-	?>
 	<div class="site-navigation__container">
 		<?php
 		wp_nav_menu(
 			array(
 				'theme_location' => 'primary',
 				'menu_id'        => 'primary-menu',
-				// 'fallback_cb'    => false,
+				'menu_class'     => 'menu--primary',
+				'fallback_cb'    => '__return_false',
 			)
 		);
 		?>
