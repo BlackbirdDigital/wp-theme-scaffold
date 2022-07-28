9b4018fb7608d98b9f9b64c35c7a0dc3458d9abd
Cory Hughart <cory@blackbird.digital>
2022-07-27 23:02:13 -0400

    term-list tag with taxonomy and linked options

---
+++ template-parts/tag/term-list.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Patrial: tag-term-list.
+ *
+ * Output a list of terms from the post. Taxonomy defaults to `post_tag`.
+ *
+ * @package davidwilkinson
+ */
+
+$defaults = array(
+	'taxonomy' => 'post_tag',
+	'element'  => 'span',
+	'linked'   => true,
+);
+
+$args = wp_parse_args( $args, $defaults );
+
+$term_list = '';
+if ( $args['linked'] ) {
+	$term_list = get_the_term_list( get_the_ID(), $args['taxonomy'], '', esc_html__( ', ', 'davidwilkinson' ), '' );
+} else {
+	$terms = get_the_terms( get_the_ID(), $args['taxonomy'] );
+	if ( is_array( $terms ) && $terms[0] instanceof WP_Term ) {
+		$terms     = wp_list_pluck( $terms, 'name' );
+		$terms     = array_map(
+			function( $term ) {
+				return sprintf( '<span>%s</span>', $term );
+			},
+			$terms
+		);
+		$term_list = implode( ', ', $terms );
+	}
+}
+
+if ( $term_list ) :
+	?>
+
+<<?php echo esc_html( $args['element'] ); ?> class="tag-term-list" data-taxonomy="<?php echo esc_attr( $args['taxonomy'] ); ?>">
+	<span class="tag-term-list__label"><?php echo esc_html_x( 'Posted in', 'post tags', 'davidwilkinson' ); ?></span>
+	<span class="tag-term-list__list"><?php echo $term_list; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></span>
+</<?php echo esc_html( $args['element'] ); ?>>
+
+	<?php
+endif;
