8c12bde1fa07331043c02bd09f16ebd3d9f23e35
Cory Hughart <cory@blackbird.digital>
2022-08-01 20:23:26 -0400

    don't output page links if there are no page links to output

--- template-parts/nav/page-links.php
+++ template-parts/nav/page-links.php
@@ -20,14 +20,20 @@ $args = wp_parse_args( $args, $defaults );
 // Ensure nested array args are merged.
 $wp_link_pages_args = wp_parse_args( $args['wp_link_pages_args'], $defaults['wp_link_pages_args'] );
 
-?>
+$page_links = wp_link_pages( $wp_link_pages_args );
+
+if ( ! empty( $page_links ) ) :
+	?>
 
 <div class="nav-page-links">
 	<nav class="nav-page-links__nav">
 		<?php // Using h2 to provide a heading to the nav section. ?>
 		<h2 class="nav-page-links__label"><?php echo esc_html( $args['label'] ); ?></h2>
 		<div class="nav-page-links__links">
-			<?php wp_link_pages( $wp_link_pages_args ); ?>
+			<?php echo $page_links; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		</div>
 	</nav>
 </div>
+
+	<?php
+endif;
